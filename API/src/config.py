67680563d0a08c28695af068f38edfb1ed3fa432
maldad <ernestoregue@hotmail.com>
2022-11-15 11:33:49 -0600

    credenciales correctas

--- API/src/config.py
+++ API/src/config.py
@@ -1,8 +1,8 @@
 class DevelopmentConfig():
     DEBUG = True
-    MYSQL_HOST = 'transitustt.mysql.database.azure.com'
+    MYSQL_HOST = 'transitus-tt.mysql.database.azure.com'
     MYSQL_USER = 'usuario'
-    MYSQL_PASSWORD = 'Mi23esJo'
+    MYSQL_PASSWORD = 'Tt202212345678'
     MYSQL_DB = 'places'
     MYSQL_PORT = 3306
 
